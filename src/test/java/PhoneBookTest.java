0d586f6b101b9d05131e6cf37d6006d930a06937
Anton_Kadyshev <fender1984@bk.ru>
2025-06-09 21:03:21 +0700

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,26 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 class PhoneBookTest {
 
+
+
+@ParameterizedTest
+@CsvSource({
+        "Petya, 231245, 1",
+        "Olga, 456789 , 2",
+        "Anton, 123456, 3"
+})
+
+    void addTest(String name, int phoneNumber, int count) {
+        PhoneBook phoneBook = new PhoneBook();
+        int result = phoneBook.add(name,phoneNumber);
+    Assertions.assertEquals(count,result);
+
+    }
 }
